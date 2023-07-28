03a60a563d4f306f72d5b3d4b0ee5048d3fdea56
Cactus <erbanku@gmail.com>
2023-07-28 16:46:20 +0800

    PWA Support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -184,5 +184,38 @@ const config = {
             },
         }),
 };
-
+// PWA Support START
+module.exports = {
+    plugins: [
+      [
+        '@docusaurus/plugin-pwa',
+        {
+          debug: true,
+          offlineModeActivationStrategies: [
+            'appInstalled',
+            'standalone',
+            'queryString',
+          ],
+          pwaHead: [
+            {
+              tagName: 'link',
+              rel: 'icon',
+              href: '/img/docusaurus.png',
+            },
+            {
+              tagName: 'link',
+              rel: 'manifest',
+              href: '/manifest.json', // your PWA manifest
+            },
+            {
+              tagName: 'meta',
+              name: 'theme-color',
+              content: 'rgb(37, 194, 160)',
+            },
+          ],
+        },
+      ],
+    ],
+};
+//PWA Support END
 module.exports = config;
