c673b93eb54d7cc8eb0f2ef49ee104579f7e4183
Cactus <erbanku@gmail.com>
2023-07-28 15:30:15 +0800

    Update settings.json

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
     // metadata like html lang. For example, if your site is Chinese, you may want
     // to replace "en" with "zh-Hans".
     i18n: {
-        defaultLocale: "zh-Hans",
+        defaultLocale: "en",
         locales: ["zh-Hans", "en"],
     },
 
