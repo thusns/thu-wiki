766cc8c87424de37314eba0b5929ca49623f4f6d
Cactus <erbanku@gmail.com>
2023-07-29 00:39:27 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,24 +97,22 @@ const config = {
                         type: 'localeDropdown',
                         position: 'right',
                         dropdownItemsAfter: [
-                          {
+                        {
                             type: 'html',
                             value: '<hr style="margin: 0.3rem 0;">',
-                          },
-                          {
+                        },
+                        {
                             href: 'https://github.com/facebook/docusaurus/issues/3526',
                             label: 'Help Us Translate',
-                          },
+                        },
                         ],
-                      },
-                      {
+                    },
+                    {
                         href: 'https://github.com/facebook/docusaurus',
                         position: 'right',
                         className: 'header-github-link',
                         'aria-label': 'GitHub repository',
-                      },
-
-                    // },
+                    },
                 ],
             },
             footer: {
@@ -189,87 +187,91 @@ const config = {
 // PWA Support START
 module.exports = {
     plugins: [
-      [
+        [
         '@docusaurus/plugin-pwa',
         {
-          pwaHead: [
-            {
-              tagName: 'link',
-              rel: 'icon',
-              href: '/img/docusaurus.png',
-            },
-            {
-              tagName: 'link',
-              rel: 'manifest',
-              href: '/manifest.json',
-            },
-            {
-              tagName: 'meta',
-              name: 'theme-color',
-              content: 'rgb(37, 194, 160)',
-            },
-            {
-              tagName: 'meta',
-              name: 'apple-mobile-web-app-capable',
-              content: 'yes',
-            },
-            {
-              tagName: 'meta',
-              name: 'apple-mobile-web-app-status-bar-style',
-              content: '#000',
-            },
-            {
-              tagName: 'link',
-              rel: 'apple-touch-icon',
-              href: '/img/docusaurus.png',
-            },
-            {
-              tagName: 'link',
-              rel: 'mask-icon',
-              href: '/img/docusaurus.svg',
-              color: 'rgb(37, 194, 160)',
-            },
-            {
-              tagName: 'meta',
-              name: 'msapplication-TileImage',
-              content: '/img/docusaurus.png',
-            },
-            {
-              tagName: 'meta',
-              name: 'msapplication-TileColor',
-              content: '#000',
-            },
-          ],
+            debug: true,
+            offlineModeActivationStrategies: [
+                'appInstalled',
+                'standalone',
+                'queryString',
+            ],
+            pwaHead: [
+                {
+                tagName: 'link',
+                rel: 'icon',
+                href: '/img/docusaurus.png',
+                },
+                {
+                tagName: 'link',
+                rel: 'manifest',
+                href: '/manifest.json',
+                },
+                {
+                tagName: 'meta',
+                name: 'theme-color',
+                content: 'rgb(37, 194, 160)',
+                },
+                {
+                tagName: 'meta',
+                name: 'apple-mobile-web-app-capable',
+                content: 'yes',
+                },
+                {
+                tagName: 'meta',
+                name: 'apple-mobile-web-app-status-bar-style',
+                content: '#000',
+                },
+                {
+                tagName: 'link',
+                rel: 'apple-touch-icon',
+                href: '/img/docusaurus.png',
+                },
+                {
+                tagName: 'link',
+                rel: 'mask-icon',
+                href: '/img/docusaurus.svg',
+                color: 'rgb(37, 194, 160)',
+                },
+                {
+                tagName: 'meta',
+                name: 'msapplication-TileImage',
+                content: '/img/docusaurus.png',
+                },
+                {
+                tagName: 'meta',
+                name: 'msapplication-TileColor',
+                content: '#000',
+                },
+                    ],
         },
-      ],
     ],
-  };
+    ],
+};
 //PWA Support END
 
 module.exports = {
     themeConfig: {
-      announcementBar: {
+        announcementBar: {
         id: 'support_us',
         content:
-          'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
+            'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
         backgroundColor: '#fafbfc',
         textColor: '#091E42',
         isCloseable: false,
-      },
     },
-  };
+    },
+};
 
 
-  module.exports = {
+module.exports = {
     themeConfig: {
-      docs: {
+    docs: {
         sidebar: {
             hideable: true,
         },
-      },
     },
-  };
-
-
+    },
+};
 
 module.exports = config;
