a6a8cbf8a0545725a0d3fb546b97720e32732f37
Cactus <erbanku@gmail.com>
2023-07-29 10:17:19 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -188,64 +188,67 @@ const config = {
 module.exports = {
     plugins: [
         [
-        '@docusaurus/plugin-pwa',
-        {
-            debug: true,
-            offlineModeActivationStrategies: [
+        // '@docusaurus/plugin-pwa',
+        'pwa',
+            {
+              // debug: isDeployPreview,
+              offlineModeActivationStrategies: [
                 'appInstalled',
                 'standalone',
                 'queryString',
-            ],
-            pwaHead: [
+              ],
+              // swRegister: false,
+              swCustom: require.resolve('./src/sw.js'),
+              pwaHead: [
                 {
-                tagName: 'link',
-                rel: 'icon',
-                href: '/img/docusaurus.png',
+                  tagName: 'link',
+                  rel: 'icon',
+                  href: 'img/docusaurus.png',
                 },
                 {
-                tagName: 'link',
-                rel: 'manifest',
-                href: '/manifest.json',
+                  tagName: 'link',
+                  rel: 'manifest',
+                  href: 'manifest.json',
                 },
                 {
-                tagName: 'meta',
-                name: 'theme-color',
-                content: 'rgb(37, 194, 160)',
+                  tagName: 'meta',
+                  name: 'theme-color',
+                  content: 'rgb(37, 194, 160)',
                 },
                 {
-                tagName: 'meta',
-                name: 'apple-mobile-web-app-capable',
-                content: 'yes',
+                  tagName: 'meta',
+                  name: 'apple-mobile-web-app-capable',
+                  content: 'yes',
                 },
                 {
-                tagName: 'meta',
-                name: 'apple-mobile-web-app-status-bar-style',
-                content: '#000',
+                  tagName: 'meta',
+                  name: 'apple-mobile-web-app-status-bar-style',
+                  content: '#000',
                 },
                 {
-                tagName: 'link',
-                rel: 'apple-touch-icon',
-                href: '/img/docusaurus.png',
+                  tagName: 'link',
+                  rel: 'apple-touch-icon',
+                  href: 'img/docusaurus.png',
                 },
                 {
-                tagName: 'link',
-                rel: 'mask-icon',
-                href: '/img/docusaurus.svg',
-                color: 'rgb(37, 194, 160)',
+                  tagName: 'link',
+                  rel: 'mask-icon',
+                  href: 'img/docusaurus.png',
+                  color: 'rgb(62, 204, 94)',
                 },
                 {
-                tagName: 'meta',
-                name: 'msapplication-TileImage',
-                content: '/img/docusaurus.png',
+                  tagName: 'meta',
+                  name: 'msapplication-TileImage',
+                  content: 'img/docusaurus.png',
                 },
                 {
-                tagName: 'meta',
-                name: 'msapplication-TileColor',
-                content: '#000',
+                  tagName: 'meta',
+                  name: 'msapplication-TileColor',
+                  content: '#000',
                 },
-                    ],
-        },
-    ],
+              ],
+            },
+          ],
     ],
 };
 //PWA Support END
