606c42f7a1331673cc12a2e4e79323e90f009d06
Cactus <erbanku@gmail.com>
2023-10-02 00:47:57 +0800

    support comments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -256,6 +256,25 @@ module.exports = {
 };
 //PWA Support END
 
+// Comment Support START
+<script src="https://giscus.app/client.js"
+        data-repo="thusns/thuwiki-giscus"
+        data-repo-id="R_kgDOKan1Cg"
+        data-category="Announcements"
+        data-category-id="DIC_kwDOKan1Cs4CZxpc"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="1"
+        data-input-position="top"
+        data-theme="preferred_color_scheme"
+        data-lang="zh-CN"
+        crossorigin="anonymous"
+        async>
+</script>
+
+// Comment Support END
+
 module.exports = {
     themeConfig: {
         announcementBar: {
