ac6d6b61d01f4fc2a363d7892655bce5848ff4d2
Cactus <79493862+erbanku@users.noreply.github.com>
2025-08-18 22:12:42 +0800

    Remove legacy src.1 homepage and features components
    Deleted all files under src.1 related to homepage, features, custom styles, and service worker, as well as the duplicate src/pages/index.js. Also refactored docusaurus.config.js to clean up PWA plugin configuration and module exports. This streamlines the codebase by removing unused legacy code and consolidating configuration.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -183,91 +183,66 @@ const config = {
 				//... other
 			},
 		}),
+	plugins: [
+		[
+			'@docusaurus/plugin-pwa',
+			{
+				debug: true,
+				offlineModeActivationStrategies: [
+					'appInstalled',
+					'standalone',
+					'queryString',
+				],
+				pwaHead: [{
+						tagName: 'link',
+						rel: 'icon',
+						href: '/img/docusaurus.png',
+					},
+					{
+						tagName: 'link',
+						rel: 'manifest',
+						href: '/manifest.json',
+					},
+					{
+						tagName: 'meta',
+						name: 'theme-color',
+						content: 'rgb(37, 194, 160)',
+					},
+					{
+						tagName: 'meta',
+						name: 'apple-mobile-web-app-capable',
+						content: 'yes',
+					},
+					{
+						tagName: 'meta',
+						name: 'apple-mobile-web-app-status-bar-style',
+						content: '#000',
+					},
+					{
+						tagName: 'link',
+						rel: 'apple-touch-icon',
+						href: '/img/docusaurus.png',
+					},
+					{
+						tagName: 'link',
+						rel: 'mask-icon',
+						href: '/img/docusaurus.svg',
+						color: 'rgb(37, 194, 160)',
+					},
+					{
+						tagName: 'meta',
+						name: 'msapplication-TileImage',
+						content: '/img/docusaurus.png',
+					},
+					{
+						tagName: 'meta',
+						name: 'msapplication-TileColor',
+						content: '#000',
+					},
+				],
+			},
+		],
+	],
 };
-// PWA Support START
-[
-	'@docusaurus/plugin-pwa',
-	{
-		debug: true,
-		offlineModeActivationStrategies: [
-			'appInstalled',
-			'standalone',
-			'queryString',
-		],
-		pwaHead: [{
-				tagName: 'link',
-				rel: 'icon',
-				href: '/img/docusaurus.png',
-			},
-			{
-				tagName: 'link',
-				rel: 'manifest',
-				href: '/manifest.json',
-			},
-			{
-				tagName: 'meta',
-				name: 'theme-color',
-				content: 'rgb(37, 194, 160)',
-			},
-			{
-				tagName: 'meta',
-				name: 'apple-mobile-web-app-capable',
-				content: 'yes',
-			},
-			{
-				tagName: 'meta',
-				name: 'apple-mobile-web-app-status-bar-style',
-				content: '#000',
-			},
-			{
-				tagName: 'link',
-				rel: 'apple-touch-icon',
-				href: '/img/docusaurus.png',
-			},
-			{
-				tagName: 'link',
-				rel: 'mask-icon',
-				href: '/img/docusaurus.svg',
-				color: 'rgb(37, 194, 160)',
-			},
-			{
-				tagName: 'meta',
-				name: 'msapplication-TileImage',
-				content: '/img/docusaurus.png',
-			},
-			{
-				tagName: 'meta',
-				name: 'msapplication-TileColor',
-				content: '#000',
-			},
-		],
-	},
-]
 
-
-//PWA Support END
-
-module.exports = {
-		themeConfig: {
-			announcementBar: {
-				id: 'support_us',
-				content: 'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
-				backgroundColor: '#fafbfc',
-				textColor: '#091E42',
-				isCloseable: false,
-			},
-		},
-	},
-
-
-	module.exports = {
-		themeConfig: {
-			docs: {
-				sidebar: {
-					hideable: true,
-				},
-			},
-		},
-	},
-
-	module.exports = config;
+module.exports = config;
